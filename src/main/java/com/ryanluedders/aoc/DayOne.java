38b9f5fa040ea3a5b1fd84649ad5202987e19d57
Ryan Luedders <ryan.luedders@gmail.com>
2016-12-12 04:36:32 +0000

    initial commit, create advent of code java project using eclipse che workspace on codeenvy

---
+++ src/main/java/com/ryanluedders/aoc/DayOne.java
@@ -0,0 +1,9 @@
+package com.ryanluedders.aoc;
+
+public class DayOne {
+    
+    public static void main(String[] argvs) {
+        System.out.println("Hello Advent of Code Day One, using Eclipse Che!");
+    }
+    
+}
---
+++ src/test/java/com/ryanluedders/aoc/DayOneTest.java
@@ -0,0 +1,11 @@
+package com.ryanluedders.aoc;
+
+import static org.junit.Assert.assertTrue;
+
+public class DayOneTest {
+
+    public void testDayOne() {
+        assertTrue(true);
+    }
+    
+}
