ad9ed4d2cb3c95cb85333c8925074b6103f0e66c
Ryan Luedders <ryan.luedders@gmail.com>
2016-12-13 19:47:05 +0000

    add some helper functions for day one problem

--- src/main/java/com/ryanluedders/aoc/dayone/DayOne.java
+++ src/main/java/com/ryanluedders/aoc/dayone/DayOne.java
@@ -8,28 +8,53 @@ import java.util.List;
 
 public class DayOne {
     
-    /**
-     * +X 0
-     * +Y 1
-     * -X 2
-     * -Y 3
-     */
+    static final int X_POSITIVE = 0;
+    static final int Y_POSITIVE = 1;
+    static final int X_NEGATIVE = 2;
+    static final int Y_NEGATIVE = 3;
+    
+    static final String TURN_RIGHT = "R";
+    static final String TURN_LEFT = "L";
     
     static class Coords {
         public int x;
         public int y;
-    }
+        
+        public Coords() {}
+        
+        public Coords(Coords coords) {
+            this.x = coords.x;
+            this.y = coords.y;
+        }
+    };
+    
+    static class Position {
+        public Coords coords;
+        public int direction;
+        
+        public Position() {
+            coords = new Coords();
+        }
+        
+        public Position(Position position) {
+            coords = new Coords(position.coords);
+            direction = position.direction;
+        }
+    };
     
     public static void main(String[] argvs) throws IOException {
         List<String> input = parseStdIn(", ");
         
-        for (String s : input) {
-            System.out.println(s);
-        }
-        
         Coords loc = new Coords();
         loc.x = 0;
         loc.y = 0;
+        
+        for (String s : input) {
+            String turnDirection = s.substring(0, 1);
+            int distance = Integer.parseInt(s.substring(1));
+            
+            System.out.println(s);
+        }
     }
     
     private static List<String> parseStdIn(String seperator) throws IOException {
@@ -44,14 +69,46 @@ public class DayOne {
         return values;
     }
     
-    static int nextDirection(int currentDirection, String move) {
-        switch (move) {
-            case "R":
+    static int nextDirection(int currentDirection, String turnDirection) {
+        switch (turnDirection) {
+            case TURN_RIGHT:
                 return Math.floorMod(currentDirection - 1, 4);
-            case "L":
+            case TURN_LEFT:
                 return Math.floorMod(currentDirection + 1, 4);
         }
         throw new IllegalArgumentException("unsupported move");
+    }
+    
+    static Coords moveDistanceInDirection(Coords startingCoords, int direction, int distance) {
+        Coords result = new Coords(startingCoords);
+        switch (direction) {
+            case X_NEGATIVE:
+                result.x -= distance;
+                break;
+            case X_POSITIVE:
+                result.x += distance;
+                break;
+            case Y_NEGATIVE:
+                result.y -= distance;
+                break;
+            case Y_POSITIVE:
+                result.y += distance;
+                break;
+            default:
+                throw new IllegalArgumentException("unsupported move");
+        }
+        return result;
+    }
+    
+    static Position makeMove(Position startingPosition, String turnDirection, int distance) {
+        Position newPosition = new Position();
+        newPosition.direction = nextDirection(startingPosition.direction, turnDirection);
+        newPosition.coords = moveDistanceInDirection(
+            startingPosition.coords,
+            newPosition.direction,
+            distance
+            );
+        return newPosition;
     }
     
 }
