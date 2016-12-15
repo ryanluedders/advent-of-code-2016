afd15266f34bdbc9d41c33061714547410051ec6
Ryan Luedders <ryan.luedders@gmail.com>
2016-12-15 04:59:02 +0000

    refactor advanced keypad, use array for keypad

--- src/main/java/com/ryanluedders/aoc/daytwo/AdvancedKeypad.java
+++ src/main/java/com/ryanluedders/aoc/daytwo/AdvancedKeypad.java
@@ -1,8 +1,5 @@
 package com.ryanluedders.aoc.daytwo;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class AdvancedKeypad {
     
@@ -20,19 +17,19 @@ public class AdvancedKeypad {
         public int y;
     }
     
-    static final List<List<String>> labels = new ArrayList<List<String>>();
+    static final String[][] labels = new String[][] {
+        { nStr, nStr, nStr, nStr, nStr, nStr, nStr},
+        { nStr, nStr, nStr, "D",  nStr, nStr, nStr},
+        { nStr, nStr, "A",  "B",  "C",  nStr, nStr},
+        { nStr, "5",  "6",  "7",  "8",  "9",  nStr},
+        { nStr, nStr, "2",  "3",  "4",  nStr, nStr},
+        { nStr, nStr, nStr, "1",  nStr, nStr, nStr},
+        { nStr, nStr, nStr, nStr, nStr, nStr, nStr},
+    };
     
     private Coord location = null;
     
-    public AdvancedKeypad() {
-        labels.add(Arrays.asList(nStr, nStr, nStr, nStr, nStr, nStr, nStr));
-        labels.add(Arrays.asList(nStr, nStr, nStr, "D",  nStr, nStr, nStr));
-        labels.add(Arrays.asList(nStr, nStr, "A",  "B",  "C",  nStr, nStr));
-        labels.add(Arrays.asList(nStr, "5",  "6",  "7",  "8",  "9",  nStr));
-        labels.add(Arrays.asList(nStr, nStr, "2",  "3",  "4",  nStr, nStr));
-        labels.add(Arrays.asList(nStr, nStr, nStr, "1",  nStr, nStr, nStr));
-        labels.add(Arrays.asList(nStr, nStr, nStr, nStr, nStr, nStr, nStr));
-        
+    public AdvancedKeypad() {        
         location = new Coord();
         location.x = 1;
         location.y = 3;
@@ -45,22 +42,22 @@ public class AdvancedKeypad {
     public void move(Direction direction) {
         switch (direction) {
             case DOWN:
-                if (labels.get(location.y - 1).get(location.x) != nStr) {
+                if (labels[location.y - 1][location.x] != nStr) {
                     location.y = location.y - 1;
                 }
                 break;
             case UP:
-                if (labels.get(location.y + 1).get(location.x) != nStr) {
+                if (labels[location.y + 1][location.x] != nStr) {
                     location.y = location.y + 1;
                 }
                 break;
             case LEFT:
-                if (labels.get(location.y).get(location.x - 1) != nStr) {
+                if (labels[location.y][location.x - 1] != nStr) {
                     location.x = location.x - 1;
                 }
                 break;
             case RIGHT:
-                if (labels.get(location.y).get(location.x + 1) != nStr) {
+                if (labels[location.y][location.x + 1] != nStr) {
                     location.x = location.x + 1;
                 }
                 break;
@@ -68,7 +65,10 @@ public class AdvancedKeypad {
     }
     
     public static String getLabel(int x, int y) {
-        return labels.get(y).get(x);
+        if (y > labels.length || x > labels[0].length) {
+            throw new IllegalArgumentException("invalid coordinate");
+        }
+        return labels[y][x];
     }
 
 }
