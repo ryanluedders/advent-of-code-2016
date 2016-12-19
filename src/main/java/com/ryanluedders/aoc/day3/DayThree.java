a22724a0f7ee6e9063614f13734419ec02523dff
Ryan Luedders <ryan.luedders@gmail.com>
2016-12-19 18:49:24 +0000

    solve day 3

--- src/main/java/com/ryanluedders/aoc/day3/DayThree.java
+++ src/main/java/com/ryanluedders/aoc/day3/DayThree.java
@@ -14,8 +14,27 @@ public class DayThree {
 
     public static void main(String[] argvs) throws IOException {
         List<List<String>> inputs = parseStdIn();
+        inputs = transformInput(inputs);
         
-        System.out.println(inputs.toString());
+        System.out.println("parsing " + Integer.toString(inputs.size()) + " rows");
+        
+        int validCount = 0;
+        
+        for (List<String> set : inputs) {
+            if (set.size() != 3) {
+                throw new IllegalArgumentException("invalid input");
+            }
+            
+            Integer a = Integer.valueOf(set.get(0));
+            Integer b = Integer.valueOf(set.get(1));
+            Integer c = Integer.valueOf(set.get(2));
+            
+            if (areSidesValid(a, b, c)) {
+                validCount += 1;
+            }
+        }
+        
+        System.out.println("valid count=" + Integer.toString(validCount));
     }
     
     private static List<List<String>> parseStdIn() throws IOException {
@@ -24,13 +43,52 @@ public class DayThree {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String line;
         while((line = br.readLine()) != null) {
-            values.add(Arrays.asList(line.trim().split("\\s+")));
-            Pattern p = Pattern.compile("^\\s*(\\d+)\\s+(\\d+)\\s+(\\d+)\\s*$");
+            Pattern p = Pattern.compile("^\\s+(\\d+)\\s+(\\d+)\\s+(\\d+)\\s*$");
             Matcher m = p.matcher(line);
-            values.add(Arrays.asList(m.group(1), m.group(2), m.group(3)));
+            if (m.matches()) {
+                values.add(Arrays.asList(m.group(1), m.group(2), m.group(3)));
+            }
         }
         
         return values;
+    }
+    
+    static List<List<String>> transformInput(List<List<String>> input) {
+        List<List<String>> result = new ArrayList<List<String>>();
+        
+        if (input.size() % 3 != 0) {
+            throw new IllegalArgumentException("invalid input");
+        }
+        
+        for (int i = 0; i < input.size() - 2; i += 3) {
+            List<String> col0 = new ArrayList<String>();
+            List<String> col1 = new ArrayList<String>();
+            List<String> col2 = new ArrayList<String>();
+            
+            col0.add(input.get(i).get(0));
+            col0.add(input.get(i+1).get(0));
+            col0.add(input.get(i+2).get(0));
+            
+            col1.add(input.get(i).get(1));
+            col1.add(input.get(i+1).get(1));
+            col1.add(input.get(i+2).get(1));
+            
+            col2.add(input.get(i).get(2));
+            col2.add(input.get(i+1).get(2));
+            col2.add(input.get(i+2).get(2));
+            
+            result.add(col0);
+            result.add(col1);
+            result.add(col2);
+        }
+        
+        return result;
+    }
+    
+    static boolean areSidesValid(int a, int b, int c) {
+        return ((a + b > c) 
+                && (a + c > b) 
+                && (b + c > a));
     }
     
 }
--- src/test/java/com/ryanluedders/aoc/day3/DayThreeTest.java
+++ src/test/java/com/ryanluedders/aoc/day3/DayThreeTest.java
@@ -1,14 +1,48 @@
 package com.ryanluedders.aoc.day3;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.Arrays;
+import java.util.List;
 
 import org.junit.Test;
 
 public class DayThreeTest {
 
     @Test
-    public void testDayThree() {
-        assertTrue(true);
+    public void testAreSidesValid_valid_input() {
+        assertEquals(true, DayThree.areSidesValid(1,3,3));
+        assertEquals(true, DayThree.areSidesValid(3,1,3));
+    }
+    
+    @Test
+    public void testAreSidesValid_invalid_input() {
+        assertEquals(false, DayThree.areSidesValid(1,1,3));
+        assertEquals(false, DayThree.areSidesValid(3,1,1));
+    }
+    
+    @Test
+    public void testTransformInput() {
+        List<List<String>> input = Arrays.asList(
+            Arrays.asList("1", "2", "3"),
+            Arrays.asList("4", "5", "6"),
+            Arrays.asList("7", "8", "9")
+        );
+        
+        List<List<String>> result = DayThree.transformInput(input);
+        
+        assertNotNull(result);
+        assertEquals(3, result.size());
+        assertEquals("1", result.get(0).get(0));
+        assertEquals("4", result.get(0).get(1));
+        assertEquals("7", result.get(0).get(2));
+        assertEquals("2", result.get(1).get(0));
+        assertEquals("5", result.get(1).get(1));
+        assertEquals("8", result.get(1).get(2));
+        assertEquals("3", result.get(2).get(0));
+        assertEquals("6", result.get(2).get(1));
+        assertEquals("9", result.get(2).get(2));
     }
     
 }
