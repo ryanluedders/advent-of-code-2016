b802aea40e528ddaf8061cb00961d3e26df8af8d
Ryan Luedders <ryan.luedders@gmail.com>
2016-12-20 14:50:30 +0000

    add initial files for day 4 solution

---
+++ src/main/java/com/ryanluedders/aoc/day4/DayFour.java
@@ -0,0 +1,36 @@
+package com.ryanluedders.aoc.day4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class DayFour {
+
+    public static void main(String[] argvs) throws IOException {
+        List<List<String>> inputs = parseStdIn();
+
+        System.out.println(inputs.toString());
+    }
+    
+    private static List<List<String>> parseStdIn() throws IOException {
+        List<List<String>> values = new ArrayList<>();
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while((line = br.readLine()) != null) {
+            Pattern p = Pattern.compile("^\\s+(\\d+)\\s+(\\d+)\\s+(\\d+)\\s*$");
+            Matcher m = p.matcher(line);
+            if (m.matches()) {
+                values.add(Arrays.asList(m.group(1), m.group(2), m.group(3)));
+            }
+        }
+        
+        return values;
+    }
+    
+}
---
+++ src/test/java/com/ryanluedders/aoc/day4/DayFourTest.java
@@ -0,0 +1,18 @@
+package com.ryanluedders.aoc.day4;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+public class DayFourTest {
+
+    @Test
+    public void testDayFour() {
+        assertEquals(true, true);
+    }
+    
+}
