f7101f1a74adde8b3e6fc6f960fa5626bcc79fdd
Ryan Luedders <ryan.luedders@gmail.com>
2017-01-03 18:21:47 +0000

    solve day 6 part 1

---
+++ src/main/java/com/ryanluedders/aoc/day6/DaySix.java
@@ -0,0 +1,83 @@
+package com.ryanluedders.aoc.day6;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class DaySix {
+    
+    static class CountingMap<T> {
+        
+        private Map<T, Integer> counts = new HashMap<T, Integer>();
+        
+        public void record(T t) {
+            if (counts.containsKey(t)) {
+                counts.put(t, counts.get(t) + 1);
+            } else {
+                counts.put(t, 1);
+            }
+        }
+        
+        public T getMostFrequent() {
+            List<Map.Entry<T, Integer>> sortedEntries = new LinkedList<Map.Entry<T, Integer>>(counts.entrySet());
+            Collections.sort(sortedEntries, new Comparator<Map.Entry<T, Integer>>() {
+                public int compare(Map.Entry<T, Integer> o1, Map.Entry<T, Integer> o2) {
+                    return (o1.getValue()).compareTo(o2.getValue());
+                }
+            });
+            return sortedEntries.get(sortedEntries.size() - 1).getKey();
+        }
+        
+        @Override
+        public String toString() {
+            return counts.toString();
+        }
+        
+    }
+
+    public static void main(String[] argvs) throws IOException, NoSuchAlgorithmException {
+        List<String> input = parseStdIn();
+        
+        int positions = input.get(0).length();
+        
+        List<CountingMap<Character>> occurrences = new ArrayList<CountingMap<Character>>();
+        for (int i = 0; i < positions; i++) {
+            occurrences.add(new CountingMap<Character>());
+        }
+            
+        for (String s : input) {
+            for (int i = 0; i < s.length(); i++) {
+                occurrences.get(i).record(s.charAt(i));
+            }
+        }
+        
+        System.out.println(occurrences.toString());
+        
+        String result = "";
+        for(CountingMap<Character> m : occurrences) {
+            result += m.getMostFrequent();
+        }
+        
+        System.out.println("result=" + result);
+    }
+ 
+    private static List<String> parseStdIn() throws IOException {
+        List<String> values = new ArrayList<>();
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while((line = br.readLine()) != null) {
+            values.add(line);
+        }
+        
+        return values;
+    }    
+}
