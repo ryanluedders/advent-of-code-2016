e5b2aae8ecab97e31e41a5a48e8c074210655d68
Ryan Luedders <ryan.luedders@gmail.com>
2017-01-27 21:23:20 -0600

    solve day 8 parts 1 and 2

--- src/main/java/com/ryanluedders/aoc/day8/DayEight.java
+++ src/main/java/com/ryanluedders/aoc/day8/DayEight.java
@@ -27,9 +27,73 @@ public class DayEight {
         public int height;
     }
     
+    static class Screen {
+        
+        private boolean segments[][];
+        
+        public Screen (int width, int height) {
+            segments = new boolean[height][width];
+        }
+        
+        public void set(int x, int y, boolean on) {
+            System.out.println("x=" + x + " y=" + y + " w=" + segments.length + " h=" + segments[0].length);
+            segments[y][x] = on;
+        }
+        
+        public boolean get(int x, int y) {
+            System.out.println("x=" + x + " y=" + y + " w=" + segments.length + " h=" + segments[0].length);
+            return segments[y][x];
+        }
+        
+        public void rotateRow(int row, int shift) {
+            int realShift = shift % segments[row].length;
+            boolean newRow[] = new boolean[segments[row].length];
+            for (int i = 0; i < segments[row].length; i++) {
+                newRow[(i + realShift) % newRow.length] = segments[row][i];
+            }
+            segments[row] = newRow;
+        }
+        
+        public void rotateColumn(int column, int shift) {
+            int realShift = shift % segments.length;
+            boolean newColumn[] = new boolean[segments.length];
+            for (int i = 0; i < segments.length; i++) {
+                newColumn[(i + realShift) % newColumn.length] = segments[i][column];
+            }
+            for (int i = 0; i < segments.length; i++) {
+                segments[i][column] = newColumn[i];
+            }
+        }
+        
+        public int getNumberLit() {
+            int result = 0;
+            for (int i = 0; i < segments.length; i++) {
+                for (int j = 0; j < segments[i].length; j++) {
+                    result += (segments[i][j] ? 1 : 0);
+                }
+            }
+            return result;
+        }
+        
+        public String toString() {
+            String result = "";
+            for (int i = 0; i < segments.length; i++) {
+                for (int j = 0; j < segments[i].length; j++) {
+                    result += (segments[i][j] ? "X" : ".");
+                    result += " ";
+                }
+                result += "\n";
+            }
+            return result;
+        }
+    }
+    
     private static Pattern RECT_PATTERN = Pattern.compile("rect ([0-9]+)x([0-9]+)");
     private static Pattern ROTATE_ROW_PATTERN = Pattern.compile("rotate row y=([0-9]+) by ([0-9]+)");
     private static Pattern ROTATE_COL_PATTERN = Pattern.compile("rotate column x=([0-9]+) by ([0-9]+)");
+    
+    private static final int SCREEN_WIDTH = 50;
+    private static final int SCREEN_HEIGHT = 6;
     
     public static void main(String[] argvs) throws IOException {
         List<String> input = parseStdIn();
@@ -37,6 +101,31 @@ public class DayEight {
         List<Operation> operations = parseCommands(input);
         
         System.out.println(operations.toString());
+        
+        Screen s = new Screen(SCREEN_WIDTH, SCREEN_HEIGHT);
+        
+        for (Operation o : operations) {
+            if (o instanceof RectOperation) {
+                RectOperation ro = (RectOperation) o;
+                for (int i = 0; i < ro.width; i++) {
+                    for (int j = 0; j < ro.height; j++) {
+                        s.set(i, j, true);
+                    }
+                }
+            }
+            if (o instanceof RotateRowOperation) {
+                RotateRowOperation ro = (RotateRowOperation) o;
+                s.rotateRow(ro.row, ro.shift);
+            }
+            if (o instanceof RotateColumnOperation) {
+                RotateColumnOperation ro = (RotateColumnOperation) o;
+                s.rotateColumn(ro.column, ro.shift);
+            }
+        }
+        
+        System.out.println("numberLit=" + s.getNumberLit() + "\n\n");
+        
+        System.out.println(s.toString());
     }
     
     static List<Operation> parseCommands(List<String> input) {
--- src/test/java/com/ryanluedders/aoc/day8/DayEightTest.java
+++ src/test/java/com/ryanluedders/aoc/day8/DayEightTest.java
@@ -12,6 +12,7 @@ import com.ryanluedders.aoc.day8.DayEight.Operation;
 import com.ryanluedders.aoc.day8.DayEight.RectOperation;
 import com.ryanluedders.aoc.day8.DayEight.RotateColumnOperation;
 import com.ryanluedders.aoc.day8.DayEight.RotateRowOperation;
+import com.ryanluedders.aoc.day8.DayEight.Screen;
 
 public class DayEightTest {
 
@@ -36,6 +37,51 @@ public class DayEightTest {
         assertTrue(result.get(2) instanceof RectOperation);
         assertEquals(7, ((RectOperation) result.get(2)).width);
         assertEquals(1, ((RectOperation) result.get(2)).height);
+    }
+    
+    @Test
+    public void testScreenOperation() {
+        Screen s = new Screen(50, 6);
+        s.set(0, 0, true);
+        s.set(0, 1, true);
+        s.set(3, 2, true);
+        s.set(3, 3, true);
+        
+        System.out.println(s.toString());
+        
+        assertEquals(4, s.getNumberLit());
+        
+        assertEquals(true, s.get(0, 0));
+        assertEquals(true, s.get(0, 1));
+        assertEquals(false, s.get(0, 2));
+        assertEquals(false, s.get(3, 1));
+        assertEquals(true, s.get(3, 2));
+        assertEquals(true, s.get(3, 3));
+        assertEquals(false, s.get(3, 4));
+        assertEquals(false, s.get(4, 3));
+        
+        s.rotateRow(1, 2);
+        System.out.println("\n\n" + s.toString());
+        assertEquals(false, s.get(0, 1));
+        assertEquals(true, s.get(2, 1));
+        
+        s.rotateColumn(3, 2);
+        System.out.println("\n\n" + s.toString());
+        assertEquals(false, s.get(3, 2));
+        assertEquals(false, s.get(3, 3));
+        assertEquals(true, s.get(3, 4));
+        assertEquals(true, s.get(3, 5));
+        
+        s.rotateColumn(3,  1);
+        System.out.println("\n\n" + s.toString());
+        assertEquals(true, s.get(3, 5));
+        assertEquals(true, s.get(3, 0));
+        
+        s.rotateRow(1, 60);
+        System.out.println("\n\n" + s.toString());
+        assertEquals(true, s.get(12, 1));
+        
+        assertEquals(4, s.getNumberLit());
     }
     
 }
